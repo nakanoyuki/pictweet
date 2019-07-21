f4c81a9a6c99eadeae6801c16ea96e9529869a0f
nakanoyuki <yukich41@gmail.com>
2019-07-21 21:13:22 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "pictweet"
-set :repo_url, "git@example.com:nakanoyuki/pictweet.git"
+set :repo_url, "git@github.com:nakanoyuki/pictweet.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
